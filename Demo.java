d9550fb238abb4de1534f9d1ad65944590e05649
Vishal Bhakare <vishalbhakare1998@gmail.com>
2025-01-06 22:20:36 +0530

    push the code

---
+++ Demo.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Calculator Program!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+
+        int sum = num1 + num2;
+        System.out.println("The sum is: " + sum);
+        
+        scanner.close();
+    }
+}
